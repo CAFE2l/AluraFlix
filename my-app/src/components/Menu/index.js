2a15907ec5ad5efcbcbfdf2ccc755aa63029f9d1
CAFÉ <pciskolargx@gmail.com>
2025-11-30 16:17:05 -0300

    copiei tudo mesmo fds

--- my-app/src/components/Menu/index.js
+++ my-app/src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
-import Button from '../Button/index.js';
-//import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
-function Menu(){
-   return (
+function Menu() {
+  return (
     <nav className="Menu">
-        <a href="/">
-         <img class="Logo" src={Logo} alt='AluraFlix Logo'/>
-        </a>
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </Link>
 
-        <Button as="a" classNamse="ButtonLink" href="/">
-            Novo Vídeo
-        </Button>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
     </nav>
-   );
-
+  );
 }
 
 export default Menu;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Desafio master blaster na descrição
+// Colocar um jogo ou brincadeira nessa página:
+const Pagina404 = () => (
+  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+    <h1>Ops, quebrou tudo :O</h1>
+    <p>
+      <a href="/">Você pode jogar ou voltar pra home :)</a>
+    </p>
+    <p>
+      Ou <a href="https://www.youtube.com/watch?v=jOAU81jdi-c&list=PLTcmLKdIkOWmeNferJ292VYKBXydGeDej">aprender a fazer o jogo</a>
+    </p>
+    {/*
+      Pessoal, quem quiser fazer o desafio do Flappy Bird, da pra usar esse iframe aqui: 
+      - https://codepen.io/omariosouto/pen/pogmdGE
+
+      E quem quiser programar o jogo:
+      - https://www.youtube.com/watch?v=jOAU81jdi-c&list=PLTcmLKdIkOWmeNferJ292VYKBXydGeDej
+    */}
+    <iframe
+      title="Flappy Bird Game"
+      src="https://mariosouto.com/flappy-bird-devsoutinho/"
+      width="340"
+      height="600" />
+  </div>
+)
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
