2b42405ca759f246ef15344f4d18ff63771a66f3
CAFÉ <pciskolargx@gmail.com>
2025-11-30 16:03:51 -0300

    imersão paia bixo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Menu from '../src/components/Menu/index';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
